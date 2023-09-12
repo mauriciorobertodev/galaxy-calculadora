d60b21959d8a1c10c8ab240bcb95398ce2790de6
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-12 17:06:46 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Avatar from "@/components/Avatar.vue";
+</script>
+
+<template>
+    <div class="bg-white h-screen w-screen flex items-center justify-center flex-col">
+        <Avatar />
+    </div>
+</template>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="h-40 w-40 bg-red-500">buuh</div>
+</template>

382977d4b8a1df2fed2457d490a2a75eadec133b
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-13 16:20:08 -0300

    funções auxiliares

---
+++ src/helpers.ts
@@ -0,0 +1,31 @@
+import { AUTHORITIES, STARS } from "./constants";
+import { Authority, AuthorityID } from "./types";
+
+export function getAuthotiryByID(id: AuthorityID): Authority {
+    return AUTHORITIES.find((a) => a.id === id) as Authority;
+}
+
+export function getAuthotiryByPoints(points: number): Authority {
+    const lastAuthority = AUTHORITIES[AUTHORITIES.length - 1];
+    if (points >= lastAuthority.points) return lastAuthority;
+
+    return AUTHORITIES.find((a, index) => a.points <= points && AUTHORITIES[index + 1].points > points) as Authority;
+}
+
+export function authorityInStars(points: number): string[] {
+    const imgs: string[] = [];
+
+    for (let i = STARS.length - 1; i >= 0; i--) {
+        const authStar = STARS[i];
+        if (authStar.points <= points) {
+            const times = Math.floor(points / authStar.points);
+            points = points - times * authStar.points;
+
+            for (let index = 0; index < times; index++) {
+                imgs.push(authStar.img);
+            }
+        }
+    }
+
+    return imgs;
+}

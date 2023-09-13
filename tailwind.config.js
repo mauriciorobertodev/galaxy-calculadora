f2439a8fe781742758eee473c31990f44af07d22
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-13 16:19:22 -0300

    configuração do tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
+        container: {
+            screens: {
+                xl: "1280px",
+            },
+        },
         extend: {
             fontFamily: {
                 sans: ["Inter", ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: colors.sky,
+            },
         },
     },
-    plugins: [],
+    plugins: [forms],
 };

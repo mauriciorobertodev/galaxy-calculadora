84e83a506d135b29b639eb136dee6246b2ffc953
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-09-13 16:23:55 -0300

    adição do path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
     plugins: [vue()],
+    base: "/galaxy-calculadora/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),

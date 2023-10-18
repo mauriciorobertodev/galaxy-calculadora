97adce5c72d517390e4364aa445f953faf0e84e8
Mauricio Roberto <75261537+mauriciorobertodev@users.noreply.github.com>
2023-10-17 21:59:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 export default defineConfig({
     plugins: [vue()],
-    base: "/galaxy-calculadora/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
